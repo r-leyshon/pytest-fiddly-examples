a1fc1851c681906e68df6b5b1b296efbe6fce571
r-leyshon <richard.leyshon@ons.gov.uk>
2024-03-25 18:33:56 +0000

    docs: Add docstrings to public functions

--- src/example_pkg/feed_characters.py
+++ src/example_pkg/feed_characters.py
@@ -1,16 +1,27 @@
-"""Helping learners understand how to work with pytest fixtures.
-
-These functions work with a dataframe with the following schema:
-"name": str,
-"fave_food": str,
-"has_munchies": bool,
-"stomach_contents": str,
-"""
+"""Helping learners understand how to work with pytest fixtures."""
 import pandas as pd
-import numpy as np
 
 
-def serve_food(df:pd.DataFrame) -> pd.DataFrame:
+def serve_food(df: pd.DataFrame) -> pd.DataFrame:
+    """Serve characters their desired food.
+
+    Iterates over a df, feeding characters if they have 'the munchies' with
+    their fave_food. If the character is not Scooby Doo or Shaggy, then update
+    their has_munchies status to False. The input df is modified inplace.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A dataframe with the following columns: "name": str, "fave_food": str,
+        "has_munchies": bool, "stomach_contents": str.
+
+    Returns
+    -------
+    pd.DataFrame
+        Updated dataframe with new statuses for stomach_contents and
+        has_munchies.
+
+    """
     for ind, row in df.iterrows():
         if row["has_munchies"]:
             # if character is hungry then feed them
@@ -28,14 +39,37 @@ def serve_food(df:pd.DataFrame) -> pd.DataFrame:
 
 
 def update_food(
-    df:pd.DataFrame,
-    fave_desserts:dict={
+    df: pd.DataFrame,
+    fave_desserts: dict = {
         "Daphne": "brownie",
         "Fred": "ice cream",
         "Scooby Doo": "apple crumble",
         "Shaggy": "pudding",
         "Velma": "banana bread",
-        }) -> pd.DataFrame:
+    },
+) -> pd.DataFrame:
+    """Update a characters favourite_food to a dessert if they have eaten.
+
+    Iterates over a df, updating the fave_food value for a character if the
+    stomach_contents are not 'empty'.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A dataframe with the following columns: "name": str, "fave_food": str,
+        "has_munchies": bool, "stomach_contents": str.
+    fave_desserts : dict, optional
+        A mapping of "name" to a replacement favourite_food, by default
+        { "Daphne": "brownie", "Fred": "ice cream",
+        "Scooby Doo": "apple crumble", "Shaggy": "pudding",
+        "Velma": "banana bread", }
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with updated fave_food values.
+
+    """
     for ind, row in df.iterrows():
         if row["stomach_contents"] != "empty":
             # character has eaten, now they should prefer a dessert
