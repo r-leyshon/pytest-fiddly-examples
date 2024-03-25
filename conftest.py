db1b6f3caeac993985c278c0d8ef3ae26da3ece5
r-leyshon <richard.leyshon@ons.gov.uk>
2024-03-25 18:49:19 +0000

    chore: Add fixtures to conftest

--- conftest.py
+++ conftest.py
@@ -2,32 +2,50 @@
 import pandas as pd
 import pytest
 
+
 @pytest.fixture(scope="session")
-def mystery_machine():
-    
-     return pd.DataFrame({
-        "name": ["Daphne", "Fred", "Scooby Doo", "Shaggy", "Velma"],
-        "fave_food": ["carrots", "beans", "scooby snacks", "burgers", "hot dogs"],
-        "has_munchies": [True] * 5,
-        "stomach_contents": ["empty"] * 5,
-    })
+def _mystery_machine():
+    """Session-scoped fixture returning pandas dataframe."""
+    return pd.DataFrame(
+        {
+            "name": ["Daphne", "Fred", "Scooby Doo", "Shaggy", "Velma"],
+            "fave_food": [
+                "carrots",
+                "beans",
+                "scooby snacks",
+                "burgers",
+                "hot dogs",
+            ],
+            "has_munchies": [True] * 5,
+            "stomach_contents": ["empty"] * 5,
+        }
+    )
 
 
 @pytest.fixture(scope="session")
-def mm_session_scoped(mystery_machine):
-    return mystery_machine
+def _mm_session_scoped(_mystery_machine):
+    """Session-scoped fixture returning the _mystery_machine dataframe.
+
+    As the src functions update their input df inplace, mm_session_scoped will
+    be modified instead of mystery_machine directly.
+
+    """
+    return _mystery_machine
 
 
 @pytest.fixture(scope="module")
-def mm_module_scoped(mystery_machine):
+def _mm_module_scoped(mystery_machine):
+    """Module-scoped mystery_machine dataframe."""
     return mystery_machine
 
 
 @pytest.fixture(scope="class")
-def mm_class_scoped(mystery_machine):
+def _mm_class_scoped(mystery_machine):
+    """Class-scoped mystery_machine dataframe."""
     return mystery_machine
 
 
 @pytest.fixture(scope="function")
-def mm_function_scoped(mystery_machine):
+def _mm_function_scoped(mystery_machine):
+    """Function-scoped mystery machine dataframe."""
     return mystery_machine
