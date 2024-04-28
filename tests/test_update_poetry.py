12e4687dae71677c994ddc2f033b10b5259a3a7f
r-leyshon <richard.leyshon@ons.gov.uk>
2024-04-28 17:38:27 +0100

    test: update_poem creates a new file in tmp_path with new content

---
+++ tests/test_update_poetry.py
@@ -0,0 +1,22 @@
+"""Tests for update_poetry module."""
+import os
+
+from example_pkg import update_poetry
+
+def test_update_poem_writes_new_pattern_to_file(tmp_path):
+    """Check that update_poem changes the poem pattern and writes to file."""
+    new_poem_path = os.path.join(tmp_path, "new_poem.txt")
+    update_poetry.update_poem(
+        poem_pth="tests/data/jack-jill-2024.txt",
+        target_pattern="glitch",
+        replacement="bug",
+        out_file=new_poem_path
+        )
+    assert os.path.exists(new_poem_path)
+    # let's check what pattern was written
+    with open(new_poem_path, "r") as f:
+        what_was_written = f.read()
+        f.close()
+    assert "glitch" not in what_was_written
+    assert "bug" in what_was_written
+    assert os.listdir(tmp_path) == ["new_poem.txt"]
