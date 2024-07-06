725e9e23dfbcd8ce0de98eeaf092679c8fe66b7b
r-leyshon <richard.leyshon@ons.gov.uk>
2024-07-06 11:09:23 +0100

    refactor: Use internals & api wrapper

--- src/example_pkg/only-joking.py
+++ src/example_pkg/only-joking.py
@@ -1,36 +1,79 @@
 """Retrieve dad jokes available."""
-from typing import Union
-
 import requests
 
-def get_joke(f:Union[str, None]=None):
-    ENDPOINT = "https://icanhazdadjoke.com/"
-    HEADERS = {
-        "User-Agent": "datasavvycorner.com (https://github.com/r-leyshon/pytest-fiddly-examples)",
-        "Accept": f,
-        # "Accept": "application/json"
-        # "Accept": "text/plain"
 
+def _query_endpoint(endp:str, usr_agent:str, f:str,) -> requests.models.Response:
+    """Utility for formatting query string & requesting endpoint."""
+    HEADERS = {
+        "User-Agent": usr_agent,
+        "Accept": f,
         }
-    resp = requests.get(ENDPOINT, headers=HEADERS)
-    if resp.ok:
-        if HEADERS["Accept"] == "application/json":
-            content = resp.json()
+    resp = requests.get(endp, headers=HEADERS)
+    return resp
+
+
+def _handle_response(r: requests.models.Response) -> str:
+    """Utility for handling reponse object & returning text content.
+
+    Parameters
+    ----------
+    r : requests.models.Response
+        Response returned from webAPI endpoint.
+
+    Raises
+    ------
+    NotImplementedError
+        Requested format `f` was not either 'text/plain' or 'application/json'. 
+    requests.HTTPError
+        HTTP error was encountered.
+    """
+    if r.ok:
+        c_type = r.headers["Content-Type"]
+        if c_type == "application/json":
+            content = r.json()
             content = content["joke"]
-        elif HEADERS["Accept"] == "text/plain":
-            content = resp.content
+        elif c_type == "text/plain":
+            content = r.text
         else:
             raise NotImplementedError(
-                "This client only accepts 'application/json' or 'text/plain' format"
+                "This client accepts 'application/json' or 'text/plain' format"
                 )
     else:
         raise requests.HTTPError(
-            f"{resp.status_code}: {resp.reason}"
+            f"{r.status_code}: {r.reason}"
         )
     return content
 
 
-get_joke()
-get_joke(f="application/json")
-type(get_joke(f="text/plain"))
+def get_joke(
+    endp:str = "https://icanhazdadjoke.com/",
+    usr_agent:str = "datasavvycorner.com (https://github.com/r-leyshon/pytest-fiddly-examples)", 
+    f:str = "text/plain",
+) -> str:
+    """Request a joke from icanhazdadjoke.com.
+
+    Ask for a joke in either plain text or JSON format. Return the joke text.
+
+    Parameters
+    ----------
+    endp : str, optional
+        Endpoint to query, by default "https://icanhazdadjoke.com/"
+    usr_agent : str, optional
+        User agent value, by default
+        "datasavvycorner.com (https://github.com/r-leyshon/pytest-fiddly-examples)"
+    f : str, optional
+        Format to request eg "application.json", by default "text/plain"
+
+    Returns
+    -------
+    str
+        Joke text.
+    """
+    r = _query_endpoint(endp=endp, usr_agent=usr_agent, f=f)
+    return _handle_response(r)
+
+
+if __name__ == "__main__":
+    for _ in range(0, 5):
+        print(get_joke())
 
